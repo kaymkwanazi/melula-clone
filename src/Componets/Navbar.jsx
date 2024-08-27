c7175dba6b60efebba3dc23e8c1e66ad040134f5
Kamogelo Mkwanazi <kamopmkwanazi@gmail.com>
2024-08-27 15:22:09 +0200

    Done

--- src/Componets/Navbar.jsx
+++ src/Componets/Navbar.jsx
@@ -15,27 +15,29 @@ export const Navbar = () => {
                 <a href= '#'>SHOP</a>
                 <a href= '#'>ABOUT</a>
              </div>
+             <div className='relative'>
                 <IoCartOutline size={32}/>
-
-                <div className='md:hidden'>
+                <p className='absolute -top-1 -right-2 bg-orange-400 text-center rounded-full text-white text-xs w-5 h-5 flex items-center justify-center'>0</p>
+             </div>
+             <div className='md:hidden'>
                     <button onClick={() => setIsOpen(!isOpen)}>
                         {isOpen ? null : <FaBars />}
                     </button>
-                </div>
-               
-                {isOpen && (
-                    <div className='fixed inset-0 flex flex-col items-center justify-center space-y-6 bg-white text-black z-50'>
-                        <div className='absolute top-5 left-5'>
-                            <button onClick={() => setIsOpen(false)}>
-                                <FaTimes size={32} />
-                            </button>
-                        </div>
-                        <a href='#'>SHOP</a>
-                        <a href='#'>ABOUT</a>
-                    </div>
-                )}
-        
             </div>
+               
+            {isOpen && (
+                <div className='fixed inset-0 flex flex-col items-center justify-center space-y-6 bg-white text-black z-50'>
+                    <div className='absolute top-5 left-5'>
+                        <button onClick={() => setIsOpen(false)}>
+                            <FaTimes size={32} />
+                        </button>
+                    </div>
+                    <a href='#'>SHOP</a>
+                    <a href='#'>ABOUT</a>
+                </div>
+            )}
+        
+        </div>
 
     </nav>
   )
